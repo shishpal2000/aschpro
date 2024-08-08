222882e3dd6d6aba112ab47f0d5649faba56dabf
aj-Amneesh <amneesh.jain@mediadynox.com>
2024-08-08 17:05:17 +0530

    some content add

--- src/components/about/AboutSection.js
+++ src/components/about/AboutSection.js
@@ -1,15 +1,17 @@
 import React from "react";
-import styles from '../../styles/about.module.css'
+import styles from "../../styles/about.module.css";
 
 const AboutSection = () => {
   return (
     <div className={styles.aboutWrapper}>
       <div className="container ">
         <p className={styles.aboutpp}>About Aschpro</p>
-        <h1 className={styles.abouthhh} style={{textTransform:'capitalize'}}>Empowering businesses personalized recruitment solutions</h1>
+        <h1 className={styles.abouthhh} style={{ textTransform: "capitalize" }}>
+          Empowering businesses with personalized recruitment solutions
+        </h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AboutSection;
